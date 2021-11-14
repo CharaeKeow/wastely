983adf537fa4ff5d70711fe6d030b445868dd26c
CharaeKeow <charaekeow11@gmail.com>
2021-11-15 04:06:43 +0800

    add Image Picker with bad UI

--- src/screens/AddItem.js
+++ src/screens/AddItem.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react"
 import {
   Button,
+  Platform,
   StyleSheet,
   Text,
   View,
 } from 'react-native'
-
 import auth from '@react-native-firebase/auth'
+
+import ImagePickerComponent from "../components/ImagePicker"
 
 export default function AddItem() {
 
@@ -15,9 +17,13 @@ export default function AddItem() {
     console.log(user.uid)
   })
 
+  //TODO: Add an empty skeleton frame so that the added image is put there. To keep UI consistent
+  //as the image will push other components downwards - 👎
+
   return (
     <View>
       <Text>Add Item</Text>
+      <ImagePickerComponent />
     </View>
   )
 }
